c42639d2d3dba39acf1a9f9348a8eef0c55d1c9c
AyemDanm <danielMMomodu@gmail.com>
2024-12-04 01:06:38 +0100

    Update2

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,22 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw Error('Jobs is not an array');
+  }
+
+  jobs.forEach((myJob) => {
+    const job = queue.create('push_notification_code_3', myJob);
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    }).on('failed', (error) => {
+      console.log(`Notification job ${job.id} failed: ${error}`);
+    }).on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+    job.save((error) => {
+      if (!error) console.log(`Notification job created: ${job.id}`);
+    });
+  });
+}
+
+module.exports = createPushNotificationsJobs;
